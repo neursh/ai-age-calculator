13328b3ce24c98d66d45ab4e5782aa272fc34bb4
Neurs <hoanghuutrung431@gmail.com>
2023-12-09 07:55:09 +0700

    Change to deploy

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Calculator from './calculator'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Calculator />
-)
